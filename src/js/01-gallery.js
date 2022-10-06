441ca4a0fc335542dc423c4cdf381a8f111f1c5b
Tetiana-Shev <azaliya939@gmail.com>
2022-10-06 21:49:14 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,31 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryElements = document.querySelector('.gallery');
+const cardsMarkUp = createCardsMarkUp(galleryItems);
+
+galleryElements.insertAdjacentHTML('beforeend', cardsMarkUp);
+galleryElements.addEventListener('click', onClick);
+
+function createCardsMarkUp(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join('');
+}
+
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+let gallery = new SimpleLightbox('.gallery__item', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+gallery.on('show.simplelightbox');
